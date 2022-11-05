1a7bb3371de397701f6ba8ad04dac8f7e2d67f74
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2022-11-04 23:51:20 -0300

    att

--- js/script.js
+++ js/script.js
@@ -15,4 +15,13 @@ let loginBtn = document.querySelector('.conta-form .login-btn');
 cadastroBtn.onclick = () =>{
     cadastroBtn.classList.add('active');
     loginBtn.classList.remove('active');
+    document.querySelector('.conta-form .login-form').classList.remove('active');
+    document.querySelector('.conta-form .cadastrar-form').classList.add('active');
+}
+
+loginBtn.onclick = () =>{
+    cadastroBtn.classList.remove('active');
+    loginBtn.classList.add('active');
+    document.querySelector('.conta-form .login-form').classList.add('active');
+    document.querySelector('.conta-form .cadastrar-form').classList.remove('active');
 }
