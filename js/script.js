891a96289bd8d5ba0a300aa91112ae1ed982daeb
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2022-11-04 23:43:37 -0300

    att

--- js/script.js
+++ js/script.js
@@ -8,3 +8,11 @@ document.querySelector('#menu-btn').onclick = () =>{
 document.querySelector('#close-navbar').onclick = () =>{
     navbar.classList.remove('active');
 }
+
+let cadastroBtn = document.querySelector('.conta-form .cadastro-btn');
+let loginBtn = document.querySelector('.conta-form .login-btn');
+
+cadastroBtn.onclick = () =>{
+    cadastroBtn.classList.add('active');
+    loginBtn.classList.remove('active');
+}
